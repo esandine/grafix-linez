a2b8f36a652f1ea0ccd9156bafd3b299a9ff241c
esandine <esandine@stuy.edu>
2017-02-15 21:02:40 -0500

    Make red

--- Driver.java
+++ Driver.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 public class Driver{
     public static void main(String[] args){
 	Grafix g = new Grafix(300,500);
-	Pixel c = new Pixel(50,150,150);
+	Pixel c = new Pixel(100,0,0);
 	g.bresLine(50,150,50,350,c);
 	g.bresLine(50,150,75,100,c);
 	g.bresLine(50,350,75,400,c);
